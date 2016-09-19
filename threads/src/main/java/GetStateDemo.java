919a398831e9ab277325b2aa5ca42c8e2ce3d2f2
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-19 21:23:48 +0300

    threads 3

---
+++ threads/src/main/java/GetStateDemo.java
@@ -0,0 +1,28 @@
+import lombok.SneakyThrows;
+
+public class GetStateDemo implements Runnable{
+
+    public void run() {
+        Thread thread = Thread.currentThread();
+        System.out.println(thread.getName() + " " + thread.getState());
+    }
+
+    @SneakyThrows
+    public static void main(String args[]) {
+        Thread thread = new Thread(new GetStateDemo());
+        System.out.println(thread.getName() + " " + thread.getState());
+
+        thread.start();
+        System.out.println(thread.getName() + " " + thread.getState());
+
+        try {
+            Thread.sleep(1_000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(thread.getName() + " " + thread.getState());
+        thread.join();
+        System.out.println(thread.getName() + " " + thread.getState());
+    }
+}
